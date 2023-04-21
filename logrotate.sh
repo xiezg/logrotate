e7f1b25cf2318ba52cb2ed573b0ccf64b3b520c1
xiezg <zgxie@aibee.com>
2023-04-21 11:38:03 +0800

    logrotate因为需要根据PID给reopen发送信号，所以需要对PID进行验证

--- logrotate.sh
+++ logrotate.sh
@@ -3,24 +3,35 @@
 # Author: xiezg
 # mail: xzghyd2008@hotmail.com
 # Created Time: 2023-04-19 15:39:25
-# Last modified: 2023-04-20 18:10:41
+# Last modified: 2023-04-21 11:29:50
 #########################################################################
 #!/bin/sh
-
 set -x
+
+#############
+#$1 需要监控轮转的日志文件，该文件是reopen的输出文件
+#$2 reopen进程的PID文件，用于通知reopen重新打开该文件
+#############
 
 LOCK_FILE=/run/containerd.lock
 MAX_FILESIZE=`expr 1024 \* 1024 \* 5`
 LOG_FILENAME=$1
+REOPEN_PID_PATH=$2
 
-while [ `ps l | grep "./reopen -p" | grep -v "grep" | awk '{print $3}' | wc -l ` -eq 0 ]
-do
-    sleep 1
-done
+function process_is_running(){
 
-REOPEN_PID=`ps l | grep "./reopen -p" | grep -v "grep" | awk '{print $3}'`
+    if [ ! -f "/proc/$1/status" ];then
+        echo 0
+        return
+    fi
 
-echo "$REOPEN_PID"
+    if [ `cat /proc/$1/status | grep -E "^Name:"| awk '{print $2}'` != "$2" ];then
+        echo 0
+        return
+    fi
+
+    echo 1
+}
 
 function fsize(){
     ls -l $1 | awk '{print $5}'
@@ -58,6 +69,15 @@ function rotate(){
         echo "delete $oldest_one"
     fi
 }
+
+while [ `process_is_running \`cat $REOPEN_PID_PATH\` reopen` -eq 0 ]
+do
+    echo "waiting reopen working"
+    sleep 1
+done
+
+REOPEN_PID=`cat $REOPEN_PID_PATH`
+echo "reopen pid: $REOPEN_PID"
 
 (
 ./flock --verbose -xn 200
